f4d4ecf9447a14ccc6f11e1dcaae13b73f4b8410
JuanPabdl <a00825316@itesm.mx>
2025-06-27 17:14:30 -0600

    style: config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const {color: defaultColors} = require('tailwindcss/defaultTheme')
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      ...defaultColors,
+      'custom-teal': '#1DCD9F',
+      'custom-dk-teal': '#158063',
+      'custom-yellow': '#F4BB41',
+      'custom-dk-gray': '#222222'
+    },
     extend: {},
   },
   plugins: [],
