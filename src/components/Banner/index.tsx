4ba0d88264bfaf24b38a4d4d92ed5bea2c01621d
JuanPabdl <a00825316@itesm.mx>
2025-07-04 17:19:27 -0600

    fix (Banner): Adjusted banner content and UI

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -5,11 +5,15 @@ const Banner = () => {
     return (
         <div className="banner-container">
             <img
-            className="" 
             src="/banner_img.jpg"
             alt="banner_img"/>
             <div className='banner-text'>
-                <h1>I´m a <span>Web developer</span> <br/>set in Mexico</h1>
+                <h1>Hi I´m <span>Juan Pablo Díaz</span></h1>
+                <p className="my-4 text-center text-sm md:text-base">
+                    I create striking web applications using modern technologies.
+                    Specialized in front-end development, I build interfaces with
+                    both functionality and appeal.
+                </p>
                 {/*Boton*/}
                 <CustomButton className='btn-default'>Let's Talk</CustomButton>
             </div>
