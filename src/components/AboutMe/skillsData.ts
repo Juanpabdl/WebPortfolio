590074608146e969928e62278cfe3da54eefd6db
JuanPabdl <a00825316@itesm.mx>
2025-07-08 17:46:53 -0600

    feat: created AboutMe component

---
+++ src/components/AboutMe/skillsData.ts
@@ -0,0 +1,23 @@
+const skillList = [
+    //Frontend
+    {name: "HTML/CSS", level: 100, category: 'frontend'},
+    {name: "Javascript", level: 90, category: 'frontend'},
+    {name: "Typescript", level: 80, category: 'frontend'},
+    {name: "React", level: 100, category: 'frontend'},
+    {name: "Tailwind CSS", level: 70, category: 'frontend'},
+    {name: "Vite", level: 75, category: 'frontend'},
+    //Backend
+    {name: "NodeJS", level: 90, category: 'backend'},
+    {name: "Express", level: 85, category: 'backend'},
+    {name: "MongoDB", level: 80, category: 'backend'},
+    {name: "SQL", level: 80, category: 'backend'},
+    {name: "PosgreSQL", level: 75, category: 'backend'},
+    {name: "Azure", level: 70, category: 'backend'},
+    //Tools
+    {name: "Git/Github", level: 90, category: 'tools'},
+    {name: "Figma", level: 70, category: 'tools'},
+    {name: "VSCode", level: 100, category: 'tools'},
+    //SoftSkills
+];
+
+export default skillList;
