7adc7ff4c1c303bc6a115b6a1991a509515ef5f4
JuanPabdl <a00825316@itesm.mx>
2025-07-10 16:03:38 -0600

    style (Home): changed spacing between Homepage components

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -14,8 +14,8 @@ const AboutMe = () => {
     );
 
     return (
-        <div id="aboutme" className="py-10 px-2  min-w-[250px]">
-            <section id="info-sec" className="mx-auto">
+        <div id="aboutme" className="py-10 px-2 min-w-[250px] mb-28 md:mb-44">
+            <section id="info-sec" className="mx-auto mb-36">
                 <h2 className="underline text-xl text-center md:text-3xl">About <span className="text-custom-yellow decoration-custom-yellow">Me</span></h2>
                 <div className="info-container">
                     <div className='text-center grid grid-cols-1 gap-4'>
