7ff92d0420cdd8c577c00c54c4ba6297fb81eea1
JuanPabdl <a00825316@itesm.mx>
2025-07-16 13:47:11 -0600

    feat: created ProjectCard and ProjectList components

---
+++ src/components/ProjectCard/index.tsx
@@ -0,0 +1,59 @@
+import CustomButton from '../CustomButton'
+import './index.css'
+
+type ProjectProps = {
+    title: string,
+    description: string,
+    image: string,
+    tags: string[],
+    demoUrl: string,
+    codeUrl: string,
+}
+
+const ProjectCard = (props: ProjectProps) => {
+    const handleClickDemo = () => {
+        window.open(props.demoUrl, '_blank')
+    }
+
+    const handleClickCode = () => {
+        window.open(props.codeUrl, '_blank')
+    }
+
+    return (
+        <div className='group project-card'>
+            <div className='h-48 bg-cover overflow-hidden'>
+                <img 
+                src={props.image} 
+                alt={props.title}
+                className='project-img'/>
+            </div>
+            <h3 className='m-3 text-lg md:text-2xl underline'>{props.title}</h3>
+            <div className='flex justify-center gap-3'>
+                {props.tags.map((tag,index) => {
+                    return(
+                        <div key={index} className='tag'>{tag}</div>
+                    )
+                })}
+            </div>
+            <p className='text-sm md:text-base text-left p-3 m-2'>{props.description}</p>
+            <div className='m-4 mt-0 flex justify-end gap-6'>
+                {props.demoUrl.length > 0 ? 
+                    (
+                        <CustomButton className='btn-default text-sm' onClick={handleClickDemo}>
+                            See Demo
+                        </CustomButton>
+                    ):(
+                        <CustomButton className='btn-blocked text-sm'>
+                            See Demo
+                        </CustomButton>
+                    )
+                }
+                <CustomButton className="btn-default text-sm" onClick={handleClickCode}>
+                    See Code
+                </CustomButton>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectCard;
---
+++ src/utils/data/projectsData.ts
@@ -0,0 +1,10 @@
+export const projectList = [
+    {
+        title: 'Inventory Site',
+        description: 'College team project in collaboration with Silma Editorial, development of an inventory application that would record merch movements amongst stores, following Scrum Workflow.',
+        tags: ['React','AWS','PostgeSQL'],
+        image: 'https://www.gacelaweb.com/wp-content/uploads/app_definicion_concepto.jpg',
+        demoUrl: '',
+        codeUrl: 'https://github.com/gpaez-ol/silma',
+    },
+]
