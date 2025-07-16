a747e2eb953fee54e1f3730013d5e0f6525ffee0
JuanPabdl <a00825316@itesm.mx>
2025-07-16 17:07:44 -0600

    fix: updated project 1 image

--- src/utils/data/projectsData.ts
+++ src/utils/data/projectsData.ts
@@ -3,7 +3,7 @@ export const projectList = [
         title: 'Inventory Site',
         description: 'College team project in collaboration with Silma Editorial, development of an inventory application that would record merch movements amongst stores, following Scrum Workflow.',
         tags: ['React','AWS','PostgeSQL'],
-        image: 'https://www.gacelaweb.com/wp-content/uploads/app_definicion_concepto.jpg',
+        image: '/projects/project_img1.jpg',
         demoUrl: '',
         codeUrl: 'https://github.com/gpaez-ol/silma',
     },
