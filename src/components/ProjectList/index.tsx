6bc7f2f8db92da0f85663c973a7dbb088d9806f9
JuanPabdl <a00825316@itesm.mx>
2025-07-17 16:23:42 -0600

    feat (ProjectList): implemented button for github navigation

--- src/components/ProjectList/index.tsx
+++ src/components/ProjectList/index.tsx
@@ -1,12 +1,13 @@
 import ProjectCard from '../ProjectCard';
 import './index.css'
 import { projectList } from '../../utils/data/projectsData';
+import CustomButton from '../CustomButton';
 
 const ProjectList = () => {
     return(
         <div id="project-container">
             <h2 className='underline'>My <span className='text-custom-yellow'>Projects</span></h2>
-            <p className='px-6 my-8 text-center text-sm md:text-base'> 
+            <p className='px-6 my-10 text-center text-sm md:text-base'> 
                 Here are some of my recent projects, each project was crafted with 
                 attention to detail, performance and user experience in mind.
             </p>
@@ -29,6 +30,9 @@ const ProjectList = () => {
                         <h3>Comming soon...</h3>
                     </div>
                 )}
+                <div className='mt-20 mx-auto'>
+                    <CustomButton className='btn-default p-4 text-sm' onClick={() => window.open('https://github.com/Juanpabdl','_blank')}>Check My Github</CustomButton>
+                </div>
         </div>
     )
 }
