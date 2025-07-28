11dca08986035b7657abc710aeceaf5da754ecb4
JuanPabdl <a00825316@itesm.mx>
2025-07-28 17:47:47 -0600

    feat: toast implementation for contact form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { ToastContainer, Bounce } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'; 
 import MyRoutes from './routes/routes'
 
 function App() {
   return (
     <>
       <MyRoutes/>
+      <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      hideProgressBar={false}
+      closeOnClick={false}
+      pauseOnHover={false}
+      draggable
+      theme="dark"
+      transition={Bounce}/>
     </>
   )
 }
