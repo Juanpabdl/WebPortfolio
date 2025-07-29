611c49324d97cf5d160fb311edb5368db9ec36c2
JuanPabdl <a00825316@itesm.mx>
2025-07-29 11:58:49 -0600

    refactor (footer): added links to outer pages and copyright

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,24 +5,29 @@ import './index.css'
 const Footer = () => {
     return (
         <footer className="footer-container">
-            <div id="dev-info">
-                <h3 className='footer-head'>Juan Pablo Díaz</h3>
-                <p className='text-sm lg:text-base'>
-                    A web designer and developer from Mexico.
-                    I strive for making websites with high visual
-                    appeal, as well as good performance.
-                </p>
+            <div className='footer-main'>
+                <div id="dev-info">
+                    <h3 className='footer-head'>Juan Pablo Díaz</h3>
+                    <p className='text-sm lg:text-base'>
+                        A web designer and developer from Mexico.
+                        I strive for making websites with high visual
+                        appeal, as well as good performance.
+                    </p>
+                </div>
+                <div id="site-links">
+                    <Link to="/">Home</Link>
+                    <HashLink smooth to="/#aboutme">About Me</HashLink>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link>
+                </div>
+                <div id="platforms">
+                    <a href='https://www.linkedin.com/in/juanpablodiazlopez' target='_blank'>LinkedIn</a>
+                    <a href='https://github.com/Juanpabdl' target='_blank'>Github</a>
+                </div>
             </div>
-            <div id="site-links">
-                <Link to="/">Home</Link>
-                <HashLink smooth to="/#aboutme">About Me</HashLink>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </div>
-            <div id="platforms">
-                <a href='#'>LinkedIn</a>
-                <a href='#'>Github</a>
-            </div>
+            <p className='footer-bottom'>
+                &copy; {new Date().getFullYear()}. by JP-Tech, All rights reserved.
+            </p>
         </footer>
     )
 }
