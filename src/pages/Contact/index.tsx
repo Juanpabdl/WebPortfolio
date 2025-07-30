36821a7fe7568d51a0e87ec76f1fd8d91f4d3cd6
JuanPabdl <a00825316@itesm.mx>
2025-07-30 14:22:04 -0600

    style: fixed pages padding to prevent gap to the sidebar menu

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
     return (
         <div className="page">
             <SideBar/>
-            <div className="mt-[50px] md:mt-0 md:ml-[80px] min-h-screen p-0 pt-3 md:pt-4 md:pl-3 flex-1">
+            <div className="mt-[50px] md:mt-0 md:ml-[80px] min-h-screen p-0 pt-3 md:pt-4 flex-1">
                 <p className="ml-5">
                     <span className="font-bold mr-2">
                         Juan Pablo Díaz López
