b91c0f409ddcbc1ea7647464e4f58a29a44ba97e
JuanPabdl <a00825316@itesm.mx>
2025-07-31 17:47:14 -0600

    fix: used scrolltotop on layout

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,13 +1,12 @@
 import SideBar from "../../components/Sidebar";
-import { Outlet, ScrollRestoration} from "react-router-dom"
-//import ScrollToTop from "../../components/ScrollToTop";
+import { Outlet } from "react-router-dom"
+import ScrollToTop from "../../components/ScrollToTop";
 const Layout = () => {
     return(
         <div className="page">
             <SideBar/>
             <Outlet/>
-            <ScrollRestoration getKey={(location) => location.pathname}/>
-            {/*<ScrollToTop/>*/}
+            <ScrollToTop/>
         </div>
     )
 }
