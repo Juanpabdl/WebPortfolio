d7da1f56b68f2a36045a37771a4f4e8ad4d4d666
JuanPabdl <a00825316@itesm.mx>
2025-08-19 17:14:42 -0600

    chore: added custom fadein animations

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -10,19 +10,19 @@ const HeroSection = () => {
     }
     
     return (
-        <div className="banner-container">
+        <div className="banner-container animate-fadeindown">
             <img
             src="/banner_img.jpg"
             alt="banner_img"/>
             <div className='banner-text'>
-                <h1>Hi I´m <span>Juan Pablo Díaz</span></h1>
-                <p className="my-4 text-center text-sm md:text-base">
+                <h1 className='animate-fadeindowndelay1'>Hi I´m <span>Juan Pablo Díaz</span></h1>
+                <p className="my-4 text-center text-sm md:text-base animate-fadeindowndelay1">
                     I create striking web applications using modern technologies.
                     Specialized in front-end development, I build interfaces with
                     both functionality and appeal.
                 </p>
                 {/*Boton*/}
-                <CustomButton className='btn-default mt-3 mx-auto w-1/2 sm:w-1/3' onClick={handleButtonClick}>
+                <CustomButton className='btn-default mt-3 mx-auto w-1/2 sm:w-1/3 animate-fadeindowndelay3' onClick={handleButtonClick}>
                     <p>See more</p>
                 </CustomButton>
             </div>
--- src/components/ProjectList/index.tsx
+++ src/components/ProjectList/index.tsx
@@ -7,12 +7,12 @@ const ProjectList = () => {
     return(
         <div id="project-container">
             <h2 className='underline'>My <span className='text-custom-yellow'>Projects</span></h2>
-            <p className='px-6 my-10 text-center text-sm md:text-base'> 
+            <p className='px-6 my-10 text-center text-sm md:text-base animate-fadeindowndelay1'> 
                 Here are some of my recent projects, each project was crafted with 
                 attention to detail, performance and user experience in mind.
             </p>
                 {projectList.length > 0 ? (
-                    <div className='project-list'>
+                    <div className='project-list animate-fadeindowndelay2'>
                         {projectList.map((item, index) => {
                             return(
                                 <ProjectCard key={index} 
@@ -26,7 +26,7 @@ const ProjectList = () => {
                         })}
                     </div>
                 ):(
-                    <div>
+                    <div className='animate-fadeindowndelay2'>
                         <h3>Comming soon...</h3>
                     </div>
                 )}
